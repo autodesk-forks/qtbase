886d1f5b69b7575c1ffb89029bab63146d4d176e
Senthil Kannan Kandaswamy <senthil.kannan.kandaswamy@autodesk.com>
2024-07-24 18:15:54 -0400

    Revert "MAYA-132611: Crash when docking the Select a light source (#102)" (#105)
    #28746 was made to use an older artifact for Maya 2025.2 to save precious hours that would have been otherwise spent on waiting for the Qt build to complete. This PR aims to solve it properly by reverting the offending change specifically. This also allows us to resolve MAYA-132068
    
    This reverts commit aaca5d7.

--- src/widgets/kernel/qwidget_p.h
+++ src/widgets/kernel/qwidget_p.h
@@ -367,8 +367,6 @@ public:
     void showChildren(bool spontaneous);
     void hideChildren(bool spontaneous);
     void setParent_sys(QWidget *parent, Qt::WindowFlags);
-    void reparentWidgetWindows(QWidget *parentWithWindow, Qt::WindowFlags windowFlags = {});
-    void reparentWidgetWindowChildren(QWidget *parentWithWindow);
     void scroll_sys(int dx, int dy);
     void scroll_sys(int dx, int dy, const QRect &r);
     void deactivateWidgetCleanup();
@@ -634,8 +632,6 @@ public:
     static void setWidgetParentHelper(QObject *widgetAsObject, QObject *newParent);
 
     std::string flagsForDumping() const override;
-
-    QWidget *closestParentWidgetWithWindowHandle() const;
 
     // Variables.
     // Regular pointers (keep them together to avoid gaps on 64 bit architectures).
